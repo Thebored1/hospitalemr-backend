74489c3a7fcc111bace35b070163b8470aa5d50d
paper <papersharma675@gmai.com>
2026-02-23 14:38:05 +0530

    Configure dj_database_url to explicitly use postgresql engine to support psycopg3 on Django 6.0

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -106,7 +106,8 @@ WSGI_APPLICATION = 'hospital_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        engine='django.db.backends.postgresql'
     )
 }
 
